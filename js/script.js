e293a2e0af75a29d9399b690ebd5a651c03fcfbe
Rosie <rosie.bradshaw3@gmail.com>
2022-11-22 18:29:20 +0100

    js empezado

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 // Esto elige un nombre al azar del array 'names'
 let names = ['Alba', 'Alexander', 'Arber', 'Arnau', 'Daniel', 'Denis', 'Fabio', 'Freddy', 'Gal', 'Hel', 'Joel', 'Lautaro', 'Manu', 'Nahuel', 'Ema', 'Ro', 'Rosie', 'Sablina', 'Sergi', 'Valeria', 'Victor'];
-function random_name(names){
-    return names[Math.floor(Math.random()*names.length)];
-}
+let choose = document.querySelector('.buttonChoose');
+let randomName = '';
 
-console.log(random_name(names));
 
+choose.addEventListener('click', () =>{
+    randomName=names[Math.floor(Math.random()*names.length)];
+    console.log(randomName);  
+    return randomName;
+    });
 
 
 //Código para añadir un nombre a una lista    https://www.youtube.com/watch?v=vLfqrEP6weo
 
+let addedName=input.value;
 function addNames(){
-    let template = names.map(name => `<li>${name}</li>`.join(','));
-    document.querySelector('ul').innerHTML = template;
+    let list = names.map(name => `<li>${name}</li>`.join(','));
+    document.querySelector('.add-new-name') = list;
 }
+console.log(names);
 
-addNames();
+console.log(addedName);
 
-let buttonAdd = document.querySelector('.buttonAdd');
-let input = document.querySelector('.input');
+let buttonAdd = document.querySelector('.button-add');
+let input = document.querySelector('input');
 
 buttonAdd.addEventListener('click', () =>{
     names.push(input.value);
