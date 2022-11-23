771687f4cd6b3ec15b855881faad020cd4c8383a
GrimMori <armigofire@gmail.com>
2022-11-23 23:08:49 +0100

    add name button functionality

--- js/testing-arnau.js
+++ js/testing-arnau.js
@@ -7,19 +7,47 @@ const namebox = document.querySelector(".added-name");
 const deadbox = document.querySelector("dead");
 const area = content.children[2];
 
-let lista = [];
+let lista = ["pepa", "josepa", "josefina", "lola", "marcelino"];
 
+// ADD
+addbutton.addEventListener("click", ()=> {
+    // declare elements
+    let div = document.createElement("div");
+    let newnamebox = document.createElement("p");
+    let newnamevalue = document.createTextNode(lista[lista.length-1]);
+    let minusbutton = document.createElement("button")
+    let icon = document.createElement("img")
+    // a fragment will store them until they can be rendered together
+    let fragment = document.createDocumentFragment();
+    
+    // populate the fragment with the elements
+    fragment.appendChild(div);
+    div.appendChild(newnamebox);
+    div.classList.add("added-name")
+    newnamebox.appendChild(newnamevalue);
+    div.appendChild(minusbutton);
+    minusbutton.classList.add("minus-button")
+    minusbutton.appendChild(icon);
+    icon.setAttribute("src", "imagenes/minus.png")
+
+    // all elements are inserted at once, as a fragment
+    content.insertBefore(fragment, area);
+
+    console.log("added name");
+})
+
+// DELETE
+deletebutton.addEventListener("click", ()=> {
+    console.log("deleted name");
+    namebox.remove();
+})
+
+// GO BACK
 backbutton.addEventListener("click", () => {
     console.log("back");
     window.history.back();
 });
 
-addbutton.addEventListener("click", ()=> {
-    console.log("added name");
-    content.insertBefore(namebox, area);
-})
+// pruebas
 
-deletebutton.addEventListener("click", ()=> {
-    console.log("deleted name");
-    namebox.remove();
-})
+newname.appendChild(newnamevalue);
