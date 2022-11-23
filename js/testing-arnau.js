c21f06498efcfea7e7b2a64f62dacee360fb3c36
GrimMori <armigofire@gmail.com>
2022-11-23 21:11:53 +0100

    added remove button functionality

--- js/testing-arnau.js
+++ js/testing-arnau.js
@@ -1,6 +1,25 @@
-let backbutton = document.querySelector(".back-button");
+const content = document.querySelector("body > section")
+const backbutton = document.querySelector(".back-button");
+const addbutton = document.querySelector("button img[src*='plus']");
+const inputnamebox = document.querySelector("input[type='text']");
+const deletebutton = document.querySelector("button img[src*='minus']");
+const namebox = document.querySelector(".added-name");
+const deadbox = document.querySelector("dead");
+const area = content.children[2];
+
+let lista = [];
 
 backbutton.addEventListener("click", () => {
-    console.log("ohla");
+    console.log("back");
     window.history.back();
 });
+
+addbutton.addEventListener("click", ()=> {
+    console.log("added name");
+    content.insertBefore(namebox, area);
+})
+
+deletebutton.addEventListener("click", ()=> {
+    console.log("deleted name");
+    namebox.remove();
+})
