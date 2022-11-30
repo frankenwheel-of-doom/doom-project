8a0301e9c244ee210851c6f7c9d09e32c36d396e
GrimMori <armigofire@gmail.com>
2022-11-30 01:24:36 +0100

    now the name of the sacrificed appears above them. rewrote the code on a new file

---
+++ js/sacrificio2.js
@@ -0,0 +1,28 @@
+let eventzone = document.querySelector("div.buttons-kill"); // container of the buttons
+let stab = document.querySelector('.stab');
+let sound1 = new Audio ('sonidos/sound-stab.mp3');
+let electrocute = document.querySelector('.electrocute');
+let sound2 = new Audio ('sonidos/sound-electrocute.mp3');
+let poison = document.querySelector('.poison');
+let sound3 = new Audio ('sonidos/sound-machine-gun.mp3');
+
+let sacrificed = JSON.parse(localStorage.getItem("cemetery")); //get all dead people
+let codername = document.querySelector("h1"); // get the h1 element
+// last person that was chosen to die is the one we are killing now
+codername.innerHTML = sacrificed[sacrificed.length-1]; // last name of all dead people appears in the h1
+
+eventzone.addEventListener('click', (e) =>{
+    console.log(e.target);
+if(e.target == stab || e.target.parentNode == stab){
+    sound1.play();
+} 
+else if(e.target == electrocute || e.target.parentNode == electrocute){
+    sound2.play();
+}
+else if(e.target == poison || e.target.parentNode == poison){
+    sound3.play();
+}
+setTimeout(function () {
+    window.location.href = "juego.html"
+}, 3000);
+}, { once: true });
